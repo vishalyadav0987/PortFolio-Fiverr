4f7e2754bde0cd44256c18834d952d08c73a3107
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2025-06-19 18:13:00 +0530

    Set token in  cookie

--- backend/generateToken/generateTokenAndSetToken.js
+++ backend/generateToken/generateTokenAndSetToken.js
@@ -7,8 +7,8 @@ const generateAndSetToken = (userId, res) => {
 
   res.cookie('token', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production', 
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    secure: true, 
+    sameSite: 'none',
     maxAge: 7 * 24 * 60 * 60 * 1000
   })
 
