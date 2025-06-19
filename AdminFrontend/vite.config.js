548fdaec9f4e4100ae826bbaa34eb8bc93048c8c
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2025-06-19 13:46:33 +0530

    Change Url after deploying Backend on-render.

--- AdminFrontend/vite.config.js
+++ AdminFrontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     port: 3000,
     proxy:{
       '/api':{
-      target: 'http://localhost:4000',
+      target: 'https://portfolio-fiverr.onrender.com',
     }
     }
   }
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     port: 5173,
     proxy:{
       '/api':{
-      target: 'http://localhost:4000',
+      target: 'https://portfolio-fiverr.onrender.com',
     }
     }
   }
