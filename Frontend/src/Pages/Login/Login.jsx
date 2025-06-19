f111e3667b7c02cb6afe6ee71d01ce7f2f4094a3
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2025-06-19 18:51:56 +0530

    chal naa fir

--- Frontend/src/Pages/Login/Login.jsx
+++ Frontend/src/Pages/Login/Login.jsx
@@ -21,7 +21,8 @@ const Login = () => {
       const response = await axios.post(
         'https://portfolio-fiverr.onrender.com/api/v1/user/sign-in',
         { email:formData.email, password:formData.password},
-        { headers: { "Content-Type": "application/json" } }
+        { headers: { "Content-Type": "application/json" } },
+        { withCredentials: true } // ✅ Ensures cookies are properly set
       );
       if (response.data.success) {
         setIsAuthenticate(true);
--- backend/generateToken/generateTokenAndSetToken.js
+++ backend/generateToken/generateTokenAndSetToken.js
@@ -10,7 +10,7 @@ const generateAndSetToken = (userId, res) => {
 
   res.cookie('token', token, {
     httpOnly: true,
-    secure: true, 
+    secure: true,
     sameSite: 'none',
     maxAge: 7 * 24 * 60 * 60 * 1000
   })
